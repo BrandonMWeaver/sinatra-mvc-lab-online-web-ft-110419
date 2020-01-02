a62472d63d6715312f2284b849c3a7b654599226
Brandon Weaver <a7xncob@gmail.com>
2020-01-02 22:28:13 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -29,7 +29,7 @@ class PigLatinizer
         word = "#{word}#{word[0]}ay"
         word[0] = ''
       end
-      piglatinized_text << word
+      piglatinized_text << word.downcase
     end
     
     return piglatinized_text.join(' ')
