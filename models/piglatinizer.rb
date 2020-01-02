ace46a55c51fbdc3c2cf868043164ddfc84def1d
Brandon Weaver <a7xncob@gmail.com>
2020-01-02 21:33:45 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,13 +3,16 @@ class PigLatinizer
   def piglatinize(text)
     piglatinized_text = ""
     words = text.split(' ')
+    
     words.each do |word|
       if word[0] =~ /[aeiou]/
         word = "#{word}way"
+      elsif
       end
       puts word
       piglatinized_text += word
     end
+    
+    return piglatinized_text
   end
-  return piglatinized_text
 end
